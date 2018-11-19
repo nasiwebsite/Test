40a41ce841c0c08b8417d975d1e7d46a95928bbf
nasiwebsite <31595474+nasiwebsite@users.noreply.github.com>
2018-11-20 04:26:01 +0700

    Update bloplayer.js

--- bloplayer.js
+++ bloplayer.js
@@ -1,6 +1,6 @@
 $(".link-lightbox").on("click", function() {
     if (window.innerHeight > 540) var a = (window.innerHeight - 540) / 2;
-    var b = '<iframe src="" width="640" height="480" id="video-embed" style="border:0;"></iframe>',
+    var b = '<iframe src="" width="640" height="480" id="mediaplayer" style="border:0;"></iframe>',
         c = '<div id="close-icon"></div>',
         d = '<div class="lightbox" style="margin-top:' + a + 'px">',
         e = '<div id="back-lightbox">',
@@ -9,7 +9,7 @@ $(".link-lightbox").on("click", function() {
         h = '<div id="ref"><a href="https://101helper.blogspot.com">Powered by 101Helper</a>',
         i = "</div></div></div></div>";
     if ($("body").append(g + f + e + d + c + b + h+i), $("#window").hide(), "youtube" == $(this).data("videosite")) var i = "http://www.blogger.com/video-play.mp4?contentId=" + $(this).data("videoid")";
-    $("#window").fadeIn(), $("#video-embed").attr("src", i), $("#close-icon").on("click", function() {
+    $("#window").fadeIn(), $("#mediaplayer").attr("src", i), $("#close-icon").on("click", function() {
         $("#window").fadeOut(1e3, function() {
             $(this).remove()
         })
